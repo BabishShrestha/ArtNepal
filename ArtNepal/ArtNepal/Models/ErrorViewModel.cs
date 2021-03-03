5aa23b2a383723e390c30128f51293b13bdcfd45
Babish Shrestha <35884235+BabishShrestha@users.noreply.github.com>
2021-03-03 12:19:37 +0545

    Initial Commit

---
+++ ArtNepal/ArtNepal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArtNepal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
